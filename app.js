f0af24d0cb88227a0aa291aa8781feecefc0bacc
Bhanvendra singh <bhanvendrasingh1998@gmail.com>
2020-12-27 13:30:21 +0530

    Full pipeline test

--- app.js
+++ app.js
@@ -1,11 +1,3 @@
-const express = require('express');
-const app = express();
-const port = 8080;
-
-app.get('/', (req, res) => res.send('Welcome To K21Academy Projects'));
-
-app.listen(port);
-console.log(`App running on http://localhost:${port}`);
 <!DOCTYPE html>
 <html lang="en">
   <head>
